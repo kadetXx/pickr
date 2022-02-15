2866d718ca20e250f4736184fd250cf1cfa2b6a6
kadetXx <techkadet@gmail.com>
2022-02-15 09:46:15 +0100

    fixed day starting at sunday bug

--- src/utils/getMonthData.ts
+++ src/utils/getMonthData.ts
@@ -8,13 +8,14 @@ export const getMonthData = (month: number, year: number) => {
 
   const firstDay = getFirstdayOfMonth(month, year);
   const numberOfDays = getNumberOfDays(month, year);
+  
 
   for (let index = 0; index < totalBlocks; index++) {
     const currentDay = getDayData({
       index,
       month,
       year,
-      firstDay: firstDay - 1, // added one to firstday because index starts at zero
+      firstDay: firstDay === 0 ? 6 : firstDay - 1, // added one to firstday because index starts at zero
       numberOfDays,
     });
 
