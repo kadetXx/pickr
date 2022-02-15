20492ba474be176d87415b311cda92e1a4d9cf0a
kadetXx <techkadet@gmail.com>
2022-02-15 20:45:56 +0100

    removed unused addons

--- src/stories/Pickr.stories.tsx
+++ src/stories/Pickr.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Pickr, PickrProps } from "../Pickr";
+import { Pickr } from "../Pickr";
 
 export default {
   title: "Pickr",
