7ce4680c4f23c7a6e9425c52c69ff3a0a52e97b3
Collins Enebeli <techkadet@gmail.com>
2026-03-04 23:54:50 +0100

    fix: disable button focus ring on touch devices

--- src/components/button/Button.styles.tsx
+++ src/components/button/Button.styles.tsx
@@ -22,17 +22,19 @@ export const StyledButton = styled.button<Pick<ButtonProps, 'disabled'>>`
   transition: background-color 0.15s ease, border-color 0.15s ease,
     box-shadow 0.15s ease;
 
-  &:hover {
-    background-color: var(--col-grey-400);
-    border-color: var(--col-grey-200);
-  }
+  @media (hover: hover) {
+    &:hover {
+      background-color: var(--col-grey-400);
+      border-color: var(--col-grey-200);
+    }
 
-  &:focus-visible {
-    outline: none;
-    box-shadow: 0 0 0 2px white, 0 0 0 4px var(--col-teal);
-  }
+    &:focus-visible {
+      outline: none;
+      box-shadow: 0 0 0 2px white, 0 0 0 4px var(--col-teal);
+    }
 
-  &:active {
-    background-color: var(--col-grey-600);
+    &:active {
+      background-color: var(--col-grey-600);
+    }
   }
 `;
